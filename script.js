f0909fa6585cf3de695b13de55d636e6b5c773c6
attilabekecs <attila.bekecs@icloud.com>
2026-02-14 15:08:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -69,3 +69,31 @@ function render(){
 
 selectPartner("Fonlak");
 selectStatus("Proformán");
+
+// JSON MENTÉS
+function saveJSON(){
+  const json = JSON.stringify(data, null, 2);
+  const blob = new Blob([json], {type:"application/json"});
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "orders_backup.json";
+  a.click();
+
+  URL.revokeObjectURL(url);
+}
+
+// JSON BETÖLTÉS
+function loadJSON(event){
+  const file = event.target.files[0];
+  if(!file) return;
+
+  const reader = new FileReader();
+  reader.onload = function(e){
+    data = JSON.parse(e.target.result);
+    render();
+  };
+  reader.readAsText(file);
+}
+
