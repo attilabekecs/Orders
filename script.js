8439465c85b9c47940dda45b2af4e7959c5cdac1
attilabekecs <attila.bekecs@icloud.com>
2026-02-14 15:52:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxAwTeKoOZccLPe9Cy-jLlNJhVl_WkBVyTgCGqXMleQWPZAjaeH1GCxbP6Fm-39CUxXdQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzTgi83h80ywtygyg8j4XtzfZbV21a4B-0wHPPTj8t54jIQDujBIBpJf0HTPsoNT2Deqw/exec";
 
 let currentPartner = "Fonlak";
 let currentStatus = "Proformán";
