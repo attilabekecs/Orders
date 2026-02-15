5bd55481089204bf847e7db73e069aad1c964d82
attilabekecs <attila.bekecs@icloud.com>
2026-02-15 08:02:01 +0100

    Update and rename script.js to js/state.js

---
+++ js/state.js
@@ -0,0 +1,6 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbzTgi83h80ywtygyg8j4XtzfZbV21a4B-0wHPPTj8t54jIQDujBIBpJf0HTPsoNT2Deqw/exec";
+
+let currentPartner = "Fonlak";
+let currentStatus = "Proformán";
+let data = [];
+let saveTimer = null;
